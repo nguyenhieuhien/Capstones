1c9e982fd7552c6310e12d08ed5aa30fb48a54bd
Phan Canh Tuan <153553535+TuanPhancANH@users.noreply.github.com>
2025-05-21 16:35:15 +0700

    Build project

---
+++ LogiSimEduProject_BE/Repositories/AccountRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Base;
+using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Repositories
+{
+    public class AccountRepository : GenericRepository<Account>
+    {
+        public AccountRepository() { }
+
+        public async Task<Account> GetUserAccount(string email, string password)
+        {
+            return await _context.Accounts.FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+        }
+
+        public async Task<List<Account>> GetAll()
+        {
+            var accounts = await _context.Accounts.ToListAsync();
+
+            return accounts;
+        }
+    }
+}
---
+++ LogiSimEduProject_BE/Services/AccountService.cs
@@ -0,0 +1,37 @@
+﻿using Repositories;
+using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IAccountService
+    {
+        Task<Account> Authenticate(string email, string password);
+        Task<List<Account>> GetAll();
+    }
+
+    public class AccountService : IAccountService
+    {
+        private readonly AccountRepository _repository;
+
+        public AccountService()
+        {
+            _repository = new AccountRepository();
+        }
+
+        public async Task<Account> Authenticate(string email, string password)
+        {
+            return await _repository.GetUserAccount(email, password);
+        }
+
+        public async Task<List<Account>> GetAll()
+        {
+            return await _repository.GetAll();
+        }
+
+    }
+}
