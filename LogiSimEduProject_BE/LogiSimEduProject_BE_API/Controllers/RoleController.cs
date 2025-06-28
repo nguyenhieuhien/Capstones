e8d38ef2c7f55a3040fd8ca0d48fd9daa2f12998
Phan Canh Tuan <153553535+TuanPhancANH@users.noreply.github.com>
2025-06-28 15:14:41 +0700

    Add register

---
+++ LogiSimEduProject_BE/LogiSimEduProject_BE_API/Controllers/RoleController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repositories.Models;
+using Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LogiSimEduProject_BE_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRoleService _service;
+        public RoleController(IRoleService service) => _service = service;
+        [HttpGet]
+        public async Task<IEnumerable<Role>> Get()
+        {
+            return await _service.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Role> Get(string id)
+        {
+            return await _service.GetById(id);
+        }
+
+        //[Authorize(Roles = "1")]
+        [HttpPost]
+        public async Task<int> Post(Role role)
+        {
+            return await _service.Create(role);
+        }
+
+        //[Authorize(Roles = "1")]
+        [HttpPut("{id}")]
+        public async Task<int> Put(Role role)
+        {
+            return await _service.Update(role);
+        }
+
+        //[Authorize(Roles = "1")]
+        [HttpDelete("{id}")]
+        public async Task<bool> Delete(string id)
+        {
+            return await _service.Delete(id);
+        }
+    }
+}
--- LogiSimEduProject_BE/Repositories/AccountRepository.cs
+++ LogiSimEduProject_BE/Repositories/AccountRepository.cs
@@ -24,6 +24,11 @@ namespace Repositories
             return await _context.Accounts.FirstOrDefaultAsync(a => a.Email == email);
         }
 
+        public async Task<Role> GetRoleByNameAsync(string roleName)
+        {
+            return await _context.Roles.FirstOrDefaultAsync(r => r.Name == roleName);
+        }
+
         public async Task InsertAsync(Account account)
         {
             await _context.Accounts.AddAsync(account);
@@ -37,6 +42,16 @@ namespace Repositories
             return accounts;
         }
 
+        public async Task<List<Account>> Search(string username, string fullname, string email, string phone)
+        {
+            var accounts = await _context.Accounts.Include(t => t.UserName).Include(t => t.FullName).Include(t => t.Email).Include(t => t.Phone).Where(tq =>
+            (tq.UserName.Contains(username) || string.IsNullOrEmpty(username)
+            && (tq.FullName.Contains(fullname)) || string.IsNullOrEmpty(fullname)
+            && (tq.Email.Contains(email)) || string.IsNullOrEmpty(email)
+            && (tq.FullName.Contains(phone)) || string.IsNullOrEmpty(phone)
+            )).ToListAsync();
 
+            return accounts;
+        }
     }
 }
