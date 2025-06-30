7c77c7ef81226a7e5499add3cffc456c2848214e
Phan Canh Tuan <153553535+TuanPhancANH@users.noreply.github.com>
2025-06-30 11:22:53 +0700

    Fix

--- LogiSimEduProject_BE/LogiSimEduProject_BE_API/Controllers/DTO/Workspace/WorkspaceDTOCreate.cs
+++ LogiSimEduProject_BE/LogiSimEduProject_BE_API/Controllers/DTO/Workspace/WorkspaceDTOCreate.cs
@@ -2,11 +2,17 @@
 {
     public class WorkspaceDTOCreate
     {
-        public string UserName { get; set; }
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Password { get; set; }
+        public Guid? OrderId { get; set; }
+
+        public Guid? OrganizationId { get; set; }
+
+        public string WorkSpaceName { get; set; }
+
+        public int? NumberOfAccount { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public string Description { get; set; }
     }
 }
 
