6f43526472539e245a0bedfd7c6f87215d93ce2c
Phan Canh Tuan <153553535+TuanPhancANH@users.noreply.github.com>
2025-08-11 13:26:35 +0700

    fix

---
+++ LogiSimEduProject_BE/LogiSimEduProject_BE_API/Controllers/CertificateTemplateController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.DTO.CertificateTemplete;
+using Services.IServices;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LogiSimEduProject_BE_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CertificateTemplateController : ControllerBase
+    {
+        private readonly ICertificateTemplateService _service;
+
+        public CertificateTemplateController(ICertificateTemplateService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("create")]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> Create([FromForm] CertificateTemplateDto request)
+        {
+            var result = await _service.Create(request);
+            if (!result.Success) return BadRequest(result.Message);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var template = await _service.GetById(id);
+            return template != null ? Ok(template) : NotFound();
+        }
+
+        [HttpGet("org/{organizationId}")]
+        public async Task<IActionResult> GetAllByOrgId(Guid organizationId)
+        {
+            var templates = await _service.GetAllByOrgId(organizationId);
+            return Ok(templates);
+        }
+
+    }
+}
---
+++ LogiSimEduProject_BE/Services/DTO/CertificateTemplete/CertificateTemplateDto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTO.CertificateTemplete
+{
+    public class CertificateTemplateDto
+    {
+        public Guid? OrganizationId { get; set; }
+        public Guid? CourseId { get; set; }
+        public string TemplateName { get; set; }
+        public IFormFile BackgroundFile { get; set; }
+        public string HtmlTemplate { get; set; }
+    }
+}
